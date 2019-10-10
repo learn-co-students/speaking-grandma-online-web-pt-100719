784f2e79c84e9ddf2a2616d54df93decac44b295
Derric Fragosa <fderric@gmail.com>
2019-10-10 21:26:57 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,9 @@ def speak_to_grandma(phrase)
  
   if phrase== "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
- 
-  elsif phrase == "".upcase
-     return "HUH! SPEAK UP, SONNY!"
+
+  elsif phrase== "".upcase
+     return "HUH?! SPEAK UP, SONNY!"
  
   else   phrase == ""
     return "NO, NOT SINCE 1938!"
