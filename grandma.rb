950bd4e79ea094ce85c93c68b2bfec05288bfa0e
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-11 18:56:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,9 @@
-
+def speak_to_grandma(string)
+   if string == string.downcase
+       return "HUH?! SPEAK UP, SONNY!"
+   elsif string == 'I LOVE YOU GRANDMA!'
+       return "I LOVE YOU TOO PUMPKIN!"
+   else
+       return "NO, NOT SINCE 1938!"
+   end
+end
